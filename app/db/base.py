21161a225d49daa21f917123bc6992a7058bc3eb
Jeison Olaya <jeisonolaya58@gmail.com>
2026-05-02 14:45:47 -0500

    feat(db): configurar conexión a la base de datos y definir schema jwt_grupo_7

--- app/db/base.py
+++ app/db/base.py
@@ -1,3 +1,8 @@
 from sqlalchemy.orm import declarative_base
+from sqlalchemy import MetaData
 
-Base = declarative_base()
+# Definimos el schema globalmente en la metadata.
+# Así, todos los modelos que hereden de 'Base' se crearán automáticamente 
+metadata = MetaData(schema="jwt_grupo_7")
+
+Base = declarative_base(metadata=metadata)
--- app/db/database.py
+++ app/db/database.py
@@ -1,12 +1,28 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from app.db.base import Base
-import os
 from dotenv import load_dotenv
 
+# Cargar las variables del archivo .env
 load_dotenv()
 
+# Obtener la URL de conexión a PostgreSQL
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+# Crear el motor de la base de datos
 engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+
+# Configurar la fábrica de sesiones
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Dependencia de FastAPI para manejar la sesión de base de datos
+def get_db():
+    """
+    Crea una nueva sesión de base de datos para cada petición (request)
+    y se asegura de cerrarla cuando la petición termina.
+    """
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
